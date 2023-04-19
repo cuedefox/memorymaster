20eba3d10dfb231f7bb2eb5364a4c60405c0c16a
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-04-19 17:22:47 -0300

    agrego dificultades al juego

--- src/containers/GameContainer.jsx
+++ src/containers/GameContainer.jsx
@@ -1,8 +1,110 @@
-import React from "react";
+import {React, useState, useEffect, useContext } from "react";
+import back from "../assets/img/cards/card.png";
+import { useNavigate, Link } from "react-router-dom";
+import { Difficulty } from "../context/Difficulty";
+
+const CARD_VALUES = ["A", "B", "C", "D", "E", "F", "G"];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 const GameContainer = () => {
-    return <>
-    </>
+    const [cards, setCards] = useState([]);
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [solvedCards, setSolvedCards] = useState([]);
+    const {difficulty} = useContext(Difficulty);
+    const [timeLeft, setTimeLeft] = useState(() => {
+        switch (difficulty) {
+            case 1:
+                return 90;
+            case 2:
+                return 60;
+            case 3:
+                return 45;
+            default: 
+                return 90;
+        }
+    });
+
+    const cargarImagen = require.context("../assets/img/cards", true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            navigate("/gameEnd");
+          } else {
+            const timerId = setTimeout(() => {
+              setTimeLeft(timeLeft - 1);
+            }, 1000);
+            return () => clearTimeout(timerId);
+          }
+    }, [timeLeft, navigate]);
+  
+    useEffect(() => {
+      const initialCards = shuffle([
+        ...CARD_VALUES,
+        ...CARD_VALUES
+      ]).map((value) => ({ value, isFlipped: false, isSolved: false }));
+      setCards(initialCards);
+    }, []);
+  
+    useEffect(() => {
+      if (flippedCards.length === 2) {
+        const [card1, card2] = flippedCards.map(({ card, index }) => ({ value: card.value, index }));
+        if (card1.value === card2.value) {
+          setSolvedCards([...solvedCards, card1, card2]);
+          setFlippedCards([]);
+        } else {
+          setTimeout(() => {
+            const newCards = [...cards];
+            newCards[card1.index].isFlipped = false;
+            newCards[card2.index].isFlipped = false;
+            setCards(newCards);
+            setFlippedCards([]);
+          }, 2000);
+        }
+      }
+    }, [flippedCards]);
+  
+    const flipCard = (cardIndex) => {
+      if (flippedCards.length === 2) {
+        return;
+      }
+      const card = cards[cardIndex];
+      if (card.isFlipped || card.isSolved) {
+        return;
+      }
+      const newCards = [...cards];
+      newCards[cardIndex] = { ...card, isFlipped: true };
+      setCards(newCards);
+      setFlippedCards([...flippedCards, { card, index: cardIndex }]);
+    };
+  
+    return <main className="game">
+        <Link to={'/'}><button className="back-home">{'< Regresar'}</button></Link>
+        <p className="time-left">Tiempo: {timeLeft}</p>
+        <div className="game-container">
+            {cards.map((card, index) => (
+            <div
+            style={{ backgroundImage: `url(${back})`}}
+            key={index}
+            className={`card ${card.isFlipped ? "flipped" : ""} ${
+              card.isSolved ? "solved" : ""
+            }`}
+            onClick={() => flipCard(index)}
+            >
+            {card.isFlipped || card.isSolved ? 
+            <img src={cargarImagen(`./${card.value}.png`)} alt={`Imagen de la carta ${card.value}`} /> : 
+            ""}
+          </div>
+        ))}
+        </div>
+    </main>
 }
 
 export default GameContainer;
--- src/containers/SelectDifficultyContainer.jsx
+++ src/containers/SelectDifficultyContainer.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import Demos from "../components/demo/Demos";
+import React, { useContext } from "react";
+import {Difficulty} from "../context/Difficulty.jsx";
+import { useNavigate, Link } from "react-router-dom";
 
 const SelectDifficultyContainer = () => {
+    const {setDifficulty} = useContext(Difficulty);
+
+    const navigate = useNavigate();
+
+    const handleDifficulty = (difficulty) => {
+        setDifficulty(difficulty);
+        console.log(difficulty);
+        navigate('/game');
+    }
+
     return <main className="select-difficulty">
-        <Demos />
+        <Link to={'/'}><button className="back-home">{'< Regresar'}</button></Link>
+        <button className="easy-button" onClick={() => handleDifficulty(1)} >Facil</button>
+        <button className="normal-button" onClick={() => handleDifficulty(2)} >Normal</button>
+        <button className="hard-button" onClick={() => handleDifficulty(3)} >Dificil</button>
     </main>
 }
 
